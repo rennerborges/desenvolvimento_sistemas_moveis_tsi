de65e3fa98140b1ae0b24d578177899deccaa869
Renner Borges <rennerferreira23@gmail.com>
2022-09-18 14:52:19 -0300

    feat: alterações visuais

--- shop_app/lib/home-screen.dart
+++ shop_app/lib/home-screen.dart
@@ -32,84 +32,102 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         centerTitle: true,
       ),
-      body: ListView.builder(
-        itemCount: _products.length,
-        itemBuilder: (context, position) {
-          Product product = _products[position];
-          return Dismissible(
-            key: Key(product.titulo),
-            background: Container(
-              color: Colors.green,
-              child: const Align(
-                alignment: Alignment(-0.9, 0.0),
-                child: Icon(
-                  Icons.edit,
-                  color: Colors.white,
-                ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+            child: const Text(
+              "Produtos",
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 25,
               ),
             ),
-            secondaryBackground: Container(
-              color: Colors.red,
-              child: const Align(
-                alignment: Alignment(0.9, 0.0),
-                child: Icon(
-                  Icons.delete,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-            onDismissed: ((direction) async {
-              if (direction == DismissDirection.endToStart) {
-                setState(() {
-                  _products.removeAt(position);
-                });
-              }
-            }),
-            confirmDismiss: ((direction) async {
-              if (direction == DismissDirection.startToEnd) {
-                Product? editedProduct = await Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => CreateProductScreen(
-                      product: product,
+          ),
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: _products.length,
+            itemBuilder: (context, position) {
+              Product product = _products[position];
+              return Dismissible(
+                key: Key(product.titulo),
+                background: Container(
+                  color: Colors.green,
+                  child: const Align(
+                    alignment: Alignment(-0.9, 0.0),
+                    child: Icon(
+                      Icons.edit,
+                      color: Colors.white,
                     ),
                   ),
-                );
+                ),
+                secondaryBackground: Container(
+                  color: Colors.red,
+                  child: const Align(
+                    alignment: Alignment(0.9, 0.0),
+                    child: Icon(
+                      Icons.delete,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                onDismissed: ((direction) async {
+                  if (direction == DismissDirection.endToStart) {
+                    setState(() {
+                      _products.removeAt(position);
+                    });
+                  }
+                }),
+                confirmDismiss: ((direction) async {
+                  if (direction == DismissDirection.startToEnd) {
+                    Product? editedProduct = await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => CreateProductScreen(
+                          product: product,
+                        ),
+                      ),
+                    );
 
-                if (editedProduct != null) {
+                    if (editedProduct != null) {
+                      setState(() {
+                        _products.removeAt(position);
+                        _products.insert(position, editedProduct);
+                      });
+                    }
+
+                    return false;
+                  }
+
                   setState(() {
                     _products.removeAt(position);
-                    _products.insert(position, editedProduct);
                   });
-                }
-
-                return false;
-              }
-
-              setState(() {
-                _products.removeAt(position);
-              });
-              return true;
-            }),
-            child: ListTile(
-              title: Text(
-                product.titulo,
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 22,
+                  return true;
+                }),
+                child: ListTile(
+                  title: Text(
+                    product.titulo,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 22,
+                      color: Colors.grey[600]!,
+                    ),
+                  ),
+                  subtitle: Text(product.descricao),
+                  trailing: Text(
+                    toReal(product.valor),
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 22,
+                      color: Colors.green,
+                    ),
+                  ),
                 ),
-              ),
-              subtitle: Text(product.descricao),
-              trailing: Text(
-                toReal(product.valor),
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 22,
-                ),
-              ),
-            ),
-          );
-        },
+              );
+            },
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Theme.of(context).primaryColor,
